a688e72109d2fe284a92dbf854ce129e9302ed07
kbc713 <>
2021-05-03 05:52:44 +0000

    I added the change fahrenheit to celsius.

--- Exercise_4_problem_1.py
+++ Exercise_4_problem_1.py
@@ -19,12 +19,17 @@
 # - Adding comments in your code and a docstring that explains how to use your `fahr_to_celsius` function (i.e., you should write the purpose of the function, parameters, and returned values)
 
 # YOUR CODE HERE
+def fahr_to_celsius(temp_fahrenheit):
+  return (temp_fahrenheit-32)/1.8
 
 # ### Problem 1 tests
-# 
+#
 # Check that the function produces correct answers for:
 # 1. What is 48° Fahrenheit in Celsius? 
+print("48° Fahrenheit in Celsius is",fahr_to_celsius(48))
+
 # 2. What about 71° Fahrenheit in Celsius?
+print("71° Fahrenheit in Celsius is",fahr_to_celsius(71))
 
 # ### Check your code
 # 
